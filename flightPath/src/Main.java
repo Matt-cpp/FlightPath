987b9445794388baa88bfa169dbb7cff0166ea3e
Boltz-Louis <boltzlouis44@gmail.com>
2026-09-09 10:34:23 +0200

    loxodromie

--- flightPath/src/Main.java
+++ flightPath/src/Main.java
@@ -18,6 +18,13 @@ public class Main {
         double distMeridien = 2*distSurMemeMeridien(teta, degLatitude);
 
         System.out.println("Distance entre A jusqu'au pole Nord puis jusqu'au méridien de B avec pour degré de latitude = " + degLatitude + " : " + distMeridien);
+        double latA = 30.0;
+        double latB = 70.0;
+        double longA = 10.0;
+        double longB = 100.0;
+
+        System.out.println(loxodromie(longA,latA,longB,latB));
+
     }
 
     public static double longueurParallele(double teta) {
@@ -42,6 +49,33 @@ public class Main {
         double distDegLatitude = 111.12;
         return (degLatitude - teta) * distDegLatitude;
     }
+
+    public static double loxodromie (double longA, double latA,double longB, double latB){
+        // si meme latitude -> division par 0 danger
+        if (latA == latB){
+            return Math.abs((longA-longB))*60*Math.cos(latA);
+        }
+        else if (longA==longB){
+            return Math.abs(latA-latB) * 60;
+        }
+        else {
+            double rv = routeVraie(longA,latA,longB,latB);
+            double res = (60 * (latA - latB)) / Math.cos(rv);
+            return Math.abs(res);
+        }
+    }
+
+    public static double routeVraie(double longA, double latA, double longB, double latB ){
+        double res;
+        double b= Math.log(Math.tan(Math.toRadians(45+ (latB/2))));
+        double a= Math.log(Math.tan(Math.toRadians(45+ (latA/2))));
+
+
+        //resultat en radiant pour eviter des conversions inutiles pour la loxodromie
+        return Math.atan((Math.toRadians(longA-longB)) /(b-a));
+    }
+
+
 
 
 }
