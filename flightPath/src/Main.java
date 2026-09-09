f46873a7d6c5c1e9570184d4ea2394a635c0b201
LPtornado <esteban.helin@uphf.fr>
2026-09-09 11:00:04 +0200

    Ajout de la methode orthodromie qui calcule la distance orthodromique entre 2 points Les paramètre d'entrés sont : phiA, lambdaA, phiB, lambdaB exprimé en degrés

--- flightPath/src/Main.java
+++ flightPath/src/Main.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
         double teta = 45;
@@ -18,6 +19,16 @@ public class Main {
         double distMeridien = 2*distSurMemeMeridien(teta, degLatitude);
 
         System.out.println("Distance entre A jusqu'au pole Nord puis jusqu'au méridien de B avec pour degré de latitude = " + degLatitude + " : " + distMeridien);
+
+        double phiA = 30;
+        double lambdaA = 10;
+        double phiB = 70;
+        double lambdaB = 100;
+
+        double distOrtho = ortohodromie(phiA, lambdaA, phiB, lambdaB);
+
+
+        System.out.println("Distance orthodromique entre point A et point B : " + distOrtho);
     }
 
     public static double longueurParallele(double teta) {
@@ -41,6 +52,26 @@ public class Main {
     public static double distSurMemeMeridien(double teta, double degLatitude) {
         double distDegLatitude = 111.12;
         return (degLatitude - teta) * distDegLatitude;
+    }
+
+    public static double ortohodromie(double phiA, double lambdaA, double phiB, double lambdaB){
+
+        double radPhiA = Math.toRadians(phiA);
+        double radLambdaA = Math.toRadians(lambdaA);
+        double radPhiB = Math.toRadians(phiB);
+        double radLambdaB = Math.toRadians(lambdaB);
+
+        double base = Math.sin(radPhiA)*Math.sin(radPhiB)+ Math.cos(radPhiA)*Math.cos(radPhiB)*Math.cos(radLambdaB-radLambdaA);
+        System.out.println("res attendu = 0.469846310393. res obtenu : "+base);
+
+        double base2 = Math.acos(base);
+        System.out.println("res attendu = 61.9756793. res obtenu :"+base2);
+
+        double dist = 60*Math.toDegrees(base2);
+
+
+        return dist;
+
     }
 
 
