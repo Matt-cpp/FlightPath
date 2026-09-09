483c9251886a09ae2a7faed106a4772c4855c9ba
louisdhainaut <louis.dhainaut@uphf.fr>
2026-09-09 09:07:23 +0200

    formules + test des questions

---
+++ flightPath/src/Main.java
@@ -0,0 +1,47 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double teta = 45;
+
+        double longueur = longueurParallele(teta);
+
+        System.out.println("Longueur du parallèle avec pour colatitude teta = " + teta + " : " + longueur);
+
+        double longitudeA = 0;
+        double longitudeB = 180;
+        double distParallele = distSurMemeParallele(longitudeA, longitudeB, teta);
+
+        System.out.println("Distance entre point A de longitude : " + longitudeA + " et le point B de longitude : " + longitudeB + " avec colatitude teta : " + distParallele );
+
+        double degLatitude = 90;
+        double distMeridien = 2*distSurMemeMeridien(teta, degLatitude);
+
+        System.out.println("Distance entre A jusqu'au pole Nord puis jusqu'au méridien de B avec pour degré de latitude = " + degLatitude + " : " + distMeridien);
+    }
+
+    public static double longueurParallele(double teta) {
+        double rayonTerre = 6371;
+        double r = rayonTerre*Math.cos(teta);
+        return 2*Math.PI*r;
+    }
+
+    public static double distSurMemeParallele(double longitudeA, double longitudeB, double teta) {
+        double rayonTerre = 6371;
+        double r = rayonTerre * Math.cos(Math.toRadians(teta));
+
+        double deltaLongitude = Math.abs(longitudeB - longitudeA);
+        if (deltaLongitude > 180) {
+            deltaLongitude = 360 - deltaLongitude;
+        }
+
+        return r * Math.toRadians(deltaLongitude);
+    }
+
+    public static double distSurMemeMeridien(double teta, double degLatitude) {
+        double distDegLatitude = 111.12;
+        return (degLatitude - teta) * distDegLatitude;
+    }
+
+
+}
